7877cbcb6b7468293e0d0d0ad3ca3a99c19e8e0d
MagalGabriel <gabrielmaaga@gmail.com>
2025-10-11 17:42:37 -0300

    mais exercicios

---
+++ ex012.py
@@ -0,0 +1,5 @@
+valor = float(input("Qual é o preço do produto? R$"))
+
+valdesc = valor - (valor * 5 / 100)
+
+print("O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}".format(valor, valdesc))
