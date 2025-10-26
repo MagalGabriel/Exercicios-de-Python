976f4f11ba2d75b3a47f1638d3e940891070df60
MagalGabriel <gabrielmaaga@gmail.com>
2025-10-26 17:07:38 -0300

    16

---
+++ ex016.py
@@ -0,0 +1,10 @@
+'''from math import trunc
+
+valor = float(input("Digite um valor: "))
+
+mod = trunc(valor)
+
+print("O valor digitado foi {} e a sua porção inteira é {}".format(valor, mod))'''
+
+valor = float(input("Digite um valor: "))
+print("O valor digitado foi {} e a sua porção inteira é {}".format(valor, int(valor)))
